f37c793671db3fc172e35ff2cdf7cb068fa4717a
HariKiran <harikiran.kante@gmail.com>
2022-06-04 01:54:16 +0530

    changes in call for papers and commitee

--- src/components/body/home.js
+++ src/components/body/home.js
@@ -10,21 +10,25 @@ const Home = () => {
         {/* <video className="head_video" autoPlay muted loop>
             <source src={bgVideo} type="video/mp4"/>
             </video> */}
-        <p className="heading">1<sup>st</sup> International Conference on Smart and Sustainable Technologies in Energy and Power Systems</p>
+        <p className="heading">
+          1<sup>st</sup> International Conference on Smart and Sustainable
+          Technologies in Energy and Power Systems
+        </p>
         <p className="ssteps">SSTEPS - 2022</p>
         {/* <p className="theme">(Theme: Sustinable Technologies for Smart Future)</p> */}
-        <p className="date"> 7<sup>th</sup> Nov  - 9<sup>th</sup> Nov, 2022</p>
+        <p className="date">
+          {" "}
+          7<sup>th</sup> - 9<sup>th</sup> Nov, 2022
+        </p>
         {/* <p className="venue"> Venue: Dept. of Electrical Engineering, Central University of Haryana, Haryana </p>
         <p className="venue"> Central University of Haryana, Haryana </p> */}
         {/* <p className="format">Event format: Virtual mode </p> */}
       </div>
       <Bar />
       <div className="content">
-        <Firstpage/>
+        <Firstpage />
       </div>
     </div>
   );
-}
+};
 export default Home;
-
-
